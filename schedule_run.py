eba0ad8925283803886c6be45628214c28e5a0ad
Viktoria <v.s.polikanova@gmail.com>
2025-04-14 16:14:19 +0700

    create task? работает

--- schedule_run.py
+++ schedule_run.py
@@ -1,4 +1,5 @@
 import asyncio
+from loguru import logger
 from app.scheduler.scheduler import start_scheduler
 from app.scheduler.rabbit_consumer import consume_schedule_events
 from app.database.init_orm import init_db
@@ -7,19 +8,17 @@ from metrics.logger import setup_logger
 setup_logger()
 
 
-async def keep_alive():
-    while True:
-        await asyncio.sleep(3600)
-
-
 async def main():
     await init_db()
 
-    await asyncio.gather(
-        consume_schedule_events(),
-        start_scheduler(),
-        keep_alive()
-    )
+    # запускаем обе корутины как фоновые задачи
+    asyncio.create_task(consume_schedule_events())
+    asyncio.create_task(start_scheduler())
+
+    logger.info("✅ Consumer и планировщик запущены")
+
+    while True:
+        await asyncio.sleep(3600)
 
 
 if __name__ == "__main__":
