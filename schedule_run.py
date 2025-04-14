ee08aa38e3dc88328fc20ac0e7b2dd2ab3bf8609
Viktoria <v.s.polikanova@gmail.com>
2025-04-14 16:10:03 +0700

    gather

--- schedule_run.py
+++ schedule_run.py
@@ -1,5 +1,4 @@
 import asyncio
-from loguru import logger
 from app.scheduler.scheduler import start_scheduler
 from app.scheduler.rabbit_consumer import consume_schedule_events
 from app.database.init_orm import init_db
@@ -8,15 +7,20 @@ from metrics.logger import setup_logger
 setup_logger()
 
 
+async def keep_alive():
+    while True:
+        await asyncio.sleep(3600)
+
+
 async def main():
     await init_db()
-    asyncio.create_task(consume_schedule_events())
-    await start_scheduler()
 
-    logger.info("✅ Планировщик и consumer запущены")
+    await asyncio.gather(
+        consume_schedule_events(),
+        start_scheduler(),
+        keep_alive()
+    )
 
-    while True:
-        await asyncio.sleep(3600)  # держим процесс живым
 
 if __name__ == "__main__":
     asyncio.run(main())
