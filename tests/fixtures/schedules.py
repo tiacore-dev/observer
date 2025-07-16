9c27230ba05fc4d71da703ad35476e2512d5251a
Viktoria <v.s.polikanova@gmail.com>
2025-07-16 17:29:50 +0700

    исправила тесты

--- tests/fixtures/schedules.py
+++ tests/fixtures/schedules.py
@@ -3,18 +3,28 @@ from uuid import uuid4
 
 import pytest
 
-from app.database.models import Bot, Chat, ChatSchedule, Prompt, TargetChat
+from app.database.models import (
+    Bot,
+    Chat,
+    ChatSchedule,
+    Prompt,
+    ScheduleStrategy,
+    ScheduleType,
+    SendStrategy,
+    TargetChat,
+)
 
 
 @pytest.fixture
 async def seed_schedule(seed_prompt: Prompt, seed_chat: Chat, seed_bot: Bot):
     schedule = await ChatSchedule.create(
+        schedule_strategy=ScheduleStrategy.ANALYSIS,
         prompt=seed_prompt,
         chat=seed_chat,
-        schedule_type="once",
+        schedule_type=ScheduleType.ONCE,
         run_at=datetime.now(timezone.utc) + timedelta(minutes=5),
         enabled=True,
-        send_strategy="fixed",
+        send_strategy=SendStrategy.FIXED,
         time_to_send="16:30:00",
         company_id=uuid4(),
         bot=seed_bot,
